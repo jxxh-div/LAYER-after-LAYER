07aa6de6ee5e45ad97ff2462994331832d926891
JaeHwan <ahhancom@gmail.com>
2020-07-23 02:43:23 +0900

    디테일수정중

--- js/array.js
+++ js/array.js
@@ -3,32 +3,52 @@ let artist_contents = [];
 let artist_info = [];
 let artist_contact = [];
 let artist_main = [];
-//left,main,aside,right,name,mail,Thumnail,insta,twitter
-//0   ,1   ,2    ,3    ,4   ,5   ,6       ,7    ,8
+
+//left,main,aside,right,name,mail,Thumnail,insta,twitter,상세페이지 이름.
+//0   ,1   ,2    ,3    ,4   ,5   ,6       ,7    ,8      ,9
+//01, 02, 03, 04 로 바꾸기
+let root = "./img/artist/"
 const artists = [
-    ["./img/artist/The71_left.png","./img/artist/The71_main.png","./img/artist/The71_aside.png","./img/artist/The71_right.png","The71_","dkbeam@naver.com","./img/Thumnail/THE71_Thumnail.png","",""],
-    ["./img/artist/GEETRUO_O_left.png","./img/artist/GEETRUO_O_main.png","./img/artist/GEETRUO_O_aside.png","./img/artist/GEETRUO_O_right.png","GEETRUO_O","GEETRUO_O@naver.com","./img/Thumnail/Thumnail_geetruo_o.png","",""],
-    ["","","","","","","./img/Thumnail/Thumnail_HEEZEY.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail__DALI-PARK.png",""],
-    ["","","","","","","./img/Thumnail/Thumnail_ADHR.png",""],
-    ["","","","","","","./img/Thumnail/Thumnail_pumpum.png",""],
-    ["","","","","","","./img/Thumnail/Thumnail_ohohaoho.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_미래의미래.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_won.png",""],
-    ["","","","","","","./img/Thumnail/Thumnail_yigyurae.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_HAKYUN.jpeg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_ttissue.png",""],
-    ["","","","","","","./img/Thumnail/Thumnail_pluto-project.jpeg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_EIM.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_0810.jpg.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_en9ook.jpg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_live-kyuseon-lee_라큐리.jpeg",""],
-    ["","","","","","","./img/Thumnail/Thumnail_waterwood_수목.png",""],
+    [`${root}The71_left.png`,"./img/artist/The71_main.png","./img/artist/The71_aside.png","./img/artist/The71_right.png","THE71_<br> 더기","dkbeam@naver.com","./img/Thumnail/THE71_Thumnail.png","https://www.instagram.com/the71_","https://twitter.com/the71__","THE71"],
+    //더기
+    ["./img/artist/GEETRUO_O_left.png","./img/artist/GEETRUO_O_main.png","./img/artist/GEETRUO_O_aside.png","./img/artist/GEETRUO_O_right.png","GEETRUO_O<br>지트루","GEETRUO_O@naver.com","./img/Thumnail/Thumnail_geetruo_o.png","","","GEETRUO_O"],
+    //지트루
+    ["","","","","HEEZEY<br>히지","heezey.bae@gmail.com","./img/Thumnail/Thumnail_HEEZEY.png","https://www.instagram.com/xheezeyx","","HEEZEY"],
+    //히지
+    ["","","","","DALI-PARK<br>박달리","dali_park@naver.com ","./img/Thumnail/Thumnail__DALI-PARK.png","https://www.instagram.com/dali__park","","DALI-PARK"],
+    //박달리
+    ["","","","","ADHR<br>어도러블호러","blue_shower@naver.com","./img/Thumnail/Thumnail_ADHR.png","https://www.instagram.com/adorable_horror","https://twitter.com/acho_acho_","ADHR"],
+    //어도러블호러
+    [`${root}pumpum_left.png`,`${root}pumpum_main.png`,`${root}pumpum_aside.png`,`${root}pumpum_right.png`,"pumpum<br>펌펌","pumypumy99@gmail.com","./img/Thumnail/Thumnail_pumpum.png","https://www.instagram.com/pumpum_rum_","https://twitter.com/pumpum_rum_","pumpum"],
+    //펌펌
+    ["","","","","ADORABLE HORROR!<br>온 아나 아노","ohohaoho.official@gmail.com ","./img/Thumnail/Thumnail_ohohaoho.jpg","https://www.instagram.com/ohohaoho","","ADORABLE HORROR!"],
+    //온 아나 아노
+    ["","","","","mirai.cool<br>미래의미래","mirainoe@naver.com","./img/Thumnail/Thumnail_mirai.jpg","https://www.instagram.com/mirai.cool","https://twitter.com/mirainoe ","미래의미래"],
+    //미래
+    ["","","","","won<br>원","940519__@hanmail.net","./img/Thumnail/Thumnail_won.png","https://www.instagram.com/44won44","https://twitter.com/44won44","won"],
+    //원
+    ["","","","","yigyurae<br>이겨레","gyurae@peachesoneuniverse.com","./img/Thumnail/Thumnail_yigyurae.jpg","https://www.instagram.com/yigyurae","","yigyurae"],
+    //이겨레
+    ["","","","","ha_qn<br>하균","desingerkim@naver.com","./img/Thumnail/Thumnail_HAKYUN.jpeg","https://www.instagram.com/ha_qn","","ha_qn"],
+    //하균
+    ["","","","","Ttissue<br>티슈","ttissue89@gmail.com","./img/Thumnail/Thumnail_ttissue.png","https://www.instagram.com/ttissue89","","Ttissue"],
+    //티슈
+    ["","","","","pluto project<br>플루토 프로젝트","tkfkd2865@naver.com","./img/Thumnail/Thumnail_pluto-project.jpeg","https://www.instagram.com/pluto.project","","pluto project"],
+    //플루토
+    ["","","","","EIM<br>준휘","june1ght6@gmail.com ","./img/Thumnail/Thumnail_EIM.png","https://www.instagram.com/ifeellikeim_alive","","EIM"],
+    //준휘
+    ["","","","","0810.jpg <br>지수","0810.jpg 96o81o@naver.com","./img/Thumnail/Thumnail_0810.jpg.jpg","https://www.instagram.com/_iggies_","","0810.jpg"],
+    //지수
+    ["","","","","en9ook<br>은국","199683@naver.com","./img/Thumnail/Thumnail_en9ook.png","https://www.instagram.com/eun9ook","","en9ook"],
+    //은국
+    ["./img/artist/lkl_left.jpeg","./img/artist/lkl_main.jpeg","./img/artist/lkl_aside.jpeg","./img/artist/lkl_right.jpeg","live kyuseon lee<br>라큐리의 컴퓨터세상","kyuseon_lee@naver.com ","./img/Thumnail/Thumnail_live-kyuseon-lee.jpeg","https://www.instagram.com/live_computer_center_lee","","라큐리의 컴퓨터세상"],
+    //라큐리
+    ["","","","","waterwood<br>수목","waterwood33@gmail.com ","./img/Thumnail/Thumnail_waterwood.png","https://www.instagram.com/_waterwood","","waterwood"],
+    //수목
     
 ]
 const save = [];
     function box(item){
-        console.log(artists[item][0])
         $(`#overlay`).fadeIn(300);
         if(item === 0 || item){
             artist_contents.push(`<div id="popup_contents">`);
@@ -38,18 +58,19 @@ const save = [];
                 artist_contents.push(`<img id="popup_right" src="${artists[item][3]}" />`);
 
                 artist_contents.push(`<div id="popup_info">`);
-                    artist_contents.push(`<h3>`+artists[item][4]+`</h3>`);
-                    artist_contents.push(`<a href="${artists[item][6]}"><img src="./img/instagram.svg" alt="instargam link" /></a>`)
-                    artist_contents.push(`<a href="${artists[item][7]}"><img src="./img/twitter.svg" alt="twitter link" /></a>`)
+                    artist_contents.push(`<p>`+artists[item][9]+`</p>`);//상세페이지 이름.
+                    artist_contents.push(`<a href="${artists[item][7]}"><img src="./img/instagram.svg" alt="instargam link" /></a>`)
+                    if(artists[item][8] !== ""){
+                        artist_contents.push(`<a href="${artists[item][8]}"><img src="./img/twitter.svg" alt="twitter link" /></a>`)
+                    }
                 artist_contents.push(`</div>`);
     
                 artist_contents.push(`<div id="popup_contact">`);
                     artist_contents.push(`<h4>CONTACT</h4>`);
-                    artist_contents.push(`<p>`+artists[item][5]+`</p>`);
+                    artist_contents.push(`<p>`+artists[item][5]+`</p>`);//이메일
                 artist_contents.push(`</div>`);
 
             artist_contents.push(`</div>`);  //**주의** </ div>라고쓰면 주석처리됨.
-
 
         }
             
@@ -60,17 +81,15 @@ $(document).ready(function() {
 
     $.each(artists, function(index, item) {
         artist_main.push(`
-        <div class="box" onclick="box(${index})" >
-            <div id="Thumbnail_name">${item[4]}</div>
-            <div>
+        <div class="box" id="box${index}" onclick="box(${index})" >
+            <div class="Thumbnail_name${index}" id="Thumbnail_name">${item[4]}</div>
+            <div id="Thumnail_img">
                 <img src="${item[6]}" />
             </div>
         </div>`);
     });
     
     $('.wrapper').html(artist_main.join(''));
-
-
 
     $('#overlay').click(function() {
         $('#overlay').fadeOut(300);
@@ -85,20 +104,19 @@ $(document).ready(function() {
         artist_info.length = 0;
         artist_contact.length = 0;
     });
-    $('.box').hover(function(){
-        $('#Thumbnail_name').animate({
-            display:"block",
-            // backgroundColor: "black",
-            
-            // opacity:"0.5",
-        },300);
-    }, function(){
-        $('#Thumbnail_name').animate({
-            backgroundColor: "",
-            opacity:"1",
-        }, 300);
-    });
-   
+
+
+    for(let i =0; i<19; i++){
+        $(`#box${i}`).hover(function(){
+            $(`.Thumbnail_name${i}`).css({
+                display:"block",
+            },500);
+        }, function(){
+            $(`.Thumbnail_name${i}`).css({
+                display:"none",
+            }, 500);
+        });
+    }
 });
 
 
