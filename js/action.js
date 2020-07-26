4a8d8035817ed03d3aa931645088b345a768e64e
JaeHwan <ahhancom@gmail.com>
2020-07-26 19:36:23 +0900

    토글버튼 수정중

--- js/action.js
+++ js/action.js
@@ -6,44 +6,70 @@ let bannerLeft=0;
 let first=1;
 let last;
 let imgCnt=0;
+let toggle = 0;
+let rotateNumber = 0;
+let rotateNumber2 = 0;
 //0 left, 1 main, 2 aside, 3right, 4name, 5contact
 const time = 500;
-
-$("#m1").animate({
-    marginTop:"18px",
-},300,function(){
-    $(this).toggleClass('rotate_m1');
-});
-$("#m2").animate({
-    marginTop:"-4px",
-    marginBottom:"-12px",
-},300,function(){
-    $(this).toggleClass('rotate_m2');
-    $("#main_menu").fadeIn(200);
-});
-$("#m3").fadeOut(200);
-
+const menuSpeed = 200;
+const m_menuSpeed = 300;
 function mobileMenu(){
-    $("#mo_menu").click(function(){
-        $("#m1").animate({
-            marginTop:"18px",
-        },300,function(){
-            $(this).toggleClass('rotate_m1');
-        });
-        $("#m2").animate({
-            marginTop:"-4px",
-            marginBottom:"-12px",
-        },300,function(){
-            $(this).toggleClass('rotate_m2');
-            $("#main_menu").fadeIn(200);
-        });
-        $("#m3").fadeOut(200);
-    });
+    
+        $("#mo_menu").on('click',function(){
+            if (toggle == 0){
+                toggle = 1;
+                $("#m1").animate({
+                    marginTop:"18px",
+                },m_menuSpeed,function(){
+                    if(rotateNumber === 0){
+                        $(this).toggleClass('rotate_m1');
+                        ++rotateNumber;
+                    }else{
+                        $(this).toggleClass('rotate_m1 rotate_m1_to');
+                    }
+                });
 
+                $("#m2").animate({
+                    marginTop:"-4px",
+                    marginBottom:"-12px",
+                },m_menuSpeed,function(){
+                    if(rotateNumber2 === 0){
+                        $(this).toggleClass('rotate_m2');
+                        ++rotateNumber2;
+                    }else{
+                        $(this).toggleClass('rotate_m2 rotate_m2_to');
+                    }
+                    $("#main_menu").fadeIn(menuSpeed);
+                });
+
+                $("#m3").fadeOut(menuSpeed);
+
+            } else{
+                toggle = 0;
+                $("#main_menu").fadeOut(m_menuSpeed,function(){
+                    $("#m1").toggleClass("rotate_m1 rotate_m1_to");
+                    $("#m2").toggleClass("rotate_m2 rotate_m2_to");
+                });
+                
+                $("#m3").delay(m_menuSpeed).fadeIn(menuSpeed);
+
+                $("#m1").delay(m_menuSpeed).animate({
+                    marginTop:"8px",
+                },m_menuSpeed);
+
+                $("#m2").delay(m_menuSpeed).animate({
+                    marginTop:"8px",
+                    marginBottom:"0px",
+                },m_menuSpeed);
+                
+            }
+        });
+
+    
 }
 $(document).ready(function() {
     loading1();
-    
+
     mobileMenu();
 
     $(".loading").click(function(){
